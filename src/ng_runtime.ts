5db4ce052ab1ac6d75ee96f947a7e3abc341483a
wsyski <wsyski@gmail.com>
2019-11-20 13:54:29 +0100

    Upgraded webpack

--- src/ng_runtime.ts
+++ src/ng_runtime.ts
@@ -1,14 +1,14 @@
 // Angular
+import '@angular/animations';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
-import '@angular/http';
+import '@angular/common/http';
 import '@angular/router';
 import '@angular/forms';
 
 // RxJS
-// import 'rxjs';
-import {Observable} from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import {Observable} from 'rxjs/internal/Observable';
+import {throwError} from 'rxjs/internal/observable/throwError';
+import {catchError, map, share} from 'rxjs/operators';
